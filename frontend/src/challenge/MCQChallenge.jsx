187bbaa374c76c602dd709bff62d63da87932ea7
utkrshydv <utkarsh4socials@gmail.com>
2025-06-12 22:39:34 +0530

    make components and structure

---
+++ frontend/src/challenge/MCQChallenge.jsx
@@ -0,0 +1,6 @@
+export function MCQChallenge(){
+  return(
+    <>
+    </>
+  )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Import your Publishable Key
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
